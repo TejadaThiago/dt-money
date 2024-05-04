62a36c4ced4ce591d7772546914eca0fdc03d36e
Thiago Tejada <thiagotejada@hotmail.com>
2024-05-04 19:04:37 -0300

    json server + context api

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyled } from "./styles/global";
 import { Trasactions } from "./pages/Trasactions";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyled />
-      
-      <Trasactions />
+      <TransactionsProvider>
+        <Trasactions />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
--- src/pages/Trasactions/index.tsx
+++ src/pages/Trasactions/index.tsx
@@ -1,13 +1,18 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 export function Trasactions() {
+  const {transactions} = useContext(TransactionsContext)
+  { console.log(new Date())}
+
   return ( 
     <div > 
       <Header />
-
       <Summary />
       
       <TransactionsContainer>
@@ -16,36 +21,23 @@ export function Trasactions() {
 
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="40%">Desenvolvimento do site</td>
-              <td>
-                <PriceHighlight variant="income">
-                  R$ 1.200,00
-                </PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2024</td>
-            </tr>
-            <tr>
-              <td width="40%">Hospedagem</td>
-              <td>
-                <PriceHighlight variant="outcome">
-                  -R$ 800,00
-                </PriceHighlight>
-              </td>
-              <td>Infraestrutura</td>
-              <td>30/05/2024</td>
-            </tr>
-            <tr>
-              <td width="40%">Comissão</td>
-              <td>
-                <PriceHighlight variant="income">
-                  R$ 200,00
-                </PriceHighlight>
-              </td>
-              <td>Bonificação</td>
-              <td>11/02/2024</td>
-            </tr>
+            {
+              transactions.map(transaction => {
+                return(
+                  <tr key={transaction.id}>
+                    <td width="40%">{transaction.description}</td>
+                    <td>
+                      <PriceHighlight variant={transaction.type}>
+                        {transaction.type === 'outcome' && '- '}
+                        {priceFormatter.format(transaction.price)}
+                      </PriceHighlight>
+                    </td>
+                    <td>{transaction.category}</td>
+                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+                  </tr>
+                )
+              })
+            }
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
